7065d9911963783247c8fd09b834e3a4b816a068
hsd-11 <hsdeshmukh11@gmail.com>
2024-06-01 00:34:51 +0530

    Create pwr2.cpp
    Runtime Error
    1088 / 1110 testcases passed
    Line 9: Char 22: runtime error: signed integer overflow: 2147483647 + 1 cannot be represented in type 'int' (solution.cpp)
    SUMMARY: UndefinedBehaviorSanitizer: undefined-behavior solution.cpp:9:22

---
+++ pwr2.cpp
@@ -0,0 +1,16 @@
+#include<math.h>
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        int count=0;
+        while(n!=0)
+        {
+            if(n&1==1)
+                count++;
+            n=n>>1;
+        }
+        if(count==1)
+            return true;
+        return false;
+    }
+};
